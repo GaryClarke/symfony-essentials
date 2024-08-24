4fda734b9dfba95a7dc21847c4af7cbde13df803
Gary Clarke <garyclarke1974@gmail.com>
2024-08-24 10:28:45 +0100

    Remove request

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController
 {
     #[Route('/', name: 'home', methods: ['GET'])]
-    public function index(Request $request): Response
+    public function index(): Response
     {
         return new Response('Hello World!');
     }
