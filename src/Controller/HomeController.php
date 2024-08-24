441815b6ec688c5c800b1b610d1871b23aa54138
Gary Clarke <garyclarke1974@gmail.com>
2024-08-24 15:54:04 +0100

    Add request and response

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController
 {
-    #[Route('/', name: 'home', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/', name: 'home', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
-        return new Response('Hello World!');
+//        dd($request->toArray());
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/html');
+        $response->setContent('<h1>Hello World</h1><p>Gary Clarke</p>');
+        return $response;
     }
 }
